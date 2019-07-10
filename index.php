a7279264880c2d9fb63198170168a08984e7498f
Tiago Neves <Tiago@MacBook-Pro-de-Tiago.local>
2019-07-10 20:49:51 +1000

    new fuctions php

--- index.php
+++ index.php
@@ -1,39 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Mindroom Student Ideas</title>
-	<link rel="stylesheet" href="style.css">
-</head>
+<?php
 
-<div class="bg-dark">
-    <div class="row row-fullWidth">
-        <h1><?php  bloginfo('description') ?> </h1>
-    </div>
-</div>
-<div class="row">
-    <div class="column">
-      <ul class="bar bar-center">
-        <?php while (have_posts()) : the_post(); ?>
-            <li>
-                <h2><?php the_title() ?> </h2> 
-            </li>
-        <?php endwhile ?>
-      </ul>
-      <a class="bar" href="<?php the_permalink?>"> Read More </a>
-    </div>
-</div>
 
-<div class="row">
-    <div class="column">
-        <?php while (have_posts()) : the_post(); ?>
+$emails = ['xx@xx.com', 'xxx@xxx.com', 'xxxx@xxxx.com', 'xxx@xxx.com'];
 
-            <h2><?php the_title() ?> </h2>
 
-            <div><?php the_excerpt() ?></div>
+function listWrapper($content){
+    echo '<li>' . $content . '</li>';
+}
 
-            <a class="a" href="<?php the_permalink?>"> Read More </a>
 
-        <?php endwhile ?>
-    </div>
-</div>
+echo '<ul>';
+foreach($emails as $email){
+    listWrapper($email);
+}
+echo '<ul>';
